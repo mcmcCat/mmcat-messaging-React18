123d4da092d38e62e8069ccea80f424d42b9fbdd
mcmcCat <526008246@qq.com>
2023-10-27 22:16:12 +0800

    更新生产环境url

--- network/index.tsx
+++ network/index.tsx
@@ -4,10 +4,10 @@ import './interceptors/interceptors';
 export const httpHost =
   process.env.NODE_ENV == 'development'
     ? 'http://localhost:3000/'
-    : 'xxx:3000/';
+    : 'http://43.139.65.158:3000/';
 export const wsHOST =
   process.env.NODE_ENV == 'development'
     ? 'http://localhost:3001/'
-    : 'xxx:3001/';
+    : 'http://43.139.65.158:3001/';
 
 export const socket = io(wsHOST);
